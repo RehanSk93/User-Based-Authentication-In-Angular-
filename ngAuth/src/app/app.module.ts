e1da89a86ac9fad10a95bb584b98c80632614d37
RehanSk93 <skazam1993@gmail.com>
2020-12-19 13:03:27 +0530

    Basic Skeleton Completed

---
+++ ngAuth/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserLoginComponent } from './login/user-login/user-login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserLoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
